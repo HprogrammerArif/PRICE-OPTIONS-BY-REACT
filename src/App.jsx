9b96d911d62d29ddbd06f0542cd4b5a89b913ddc
HprogrammerArif <programmerhabluarif@gmail.com>
2024-03-20 19:46:08 +0600

    added nav section manually

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import NavBar from "./Components/Navbar/NavBar";
+// import DaisyNav from "./Components/DaisyNav/DaisyNav";
 
 function App() {
+
+  
   return (
     <>
+      {/* <DaisyNav></DaisyNav> */}
+      <NavBar></NavBar>
       <h1 className="text-6xl bg-cyan-600">Vite + React</h1>
     </>
   );
