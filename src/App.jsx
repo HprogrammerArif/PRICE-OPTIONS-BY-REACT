8117ce78e208ae1c708cc7bf417e25e48e77ceab
HprogrammerArif <programmerhabluarif@gmail.com>
2024-03-21 16:11:02 +0600

    impliment axios for the first time and added another bar chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import LineChart from "./Components/LineChart/LineChart";
 import NavBar from "./Components/Navbar/NavBar";
+import Phones from "./Components/Phones/Phones";
 import PriceOptions from "./Components/PriceOptions/PriceOptions";
 // import DaisyNav from "./Components/DaisyNav/DaisyNav";
 // import Navbar2 from "./Components/Navbar/Navbar2";
@@ -16,6 +17,7 @@ function App() {
       <h1 className="text-6xl bg-cyan-600">Vite + React</h1>
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <Phones></Phones>
     </>
   );
 }
